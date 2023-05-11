fb1dd319a11a2b237a71fd82d4c3b66b51df6074
Roman Potemin <poteminr@gmail.com>
2023-05-11 15:46:35 +0700

    add train test split for instruct dataset

--- flat_utils/instruct_dataset.py
+++ flat_utils/instruct_dataset.py
@@ -1,6 +1,7 @@
 import numpy as np
 from tqdm import tqdm
 from corus import rudrec, load_rudrec
+from sklearn.model_selection import train_test_split
 from flat_utils.instruct_utils import SEP_SYMBOL, ENTITY_TYPES, entity_type_to_instruction
 
 
@@ -32,10 +33,29 @@ def create_instructions_for_record(record: rudrec.RuDReCRecord) -> list[dict[str
     return record_instructions
 
 
-def create_instruct_dataset(filepath: str) -> list[dict[str, str]]:
+def create_instruct_dataset(filepath: str) -> np.ndarray[dict[str, str]]:
     instructions = []
     rudrec_dataset = download_dataset(filepath)
     for record in tqdm(rudrec_dataset):
         instructions = np.concatenate((instructions, create_instructions_for_record(record)))
     
     return instructions
+
+
+def create_train_test_instruct_datasets(
+        filepath: str,
+        test_size: float = 0.3,
+        random_seed: int = 42
+) -> tuple[np.ndarray[dict[str, str]]]:
+    
+    train_instructions, test_instructions = [], []
+    rudrec_dataset = download_dataset(filepath)
+    train_dataset, test_dataset = train_test_split(rudrec_dataset, test_size=test_size, random_state=random_seed)
+
+    for record in tqdm(train_dataset):
+        train_instructions = np.concatenate((train_instructions, create_instructions_for_record(record)))
+        
+    for record in tqdm(test_dataset):
+        test_instructions = np.concatenate((test_instructions, create_instructions_for_record(record)))
+    
+    return train_instructions, test_instructions
