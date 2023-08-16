f69e510caf13b2b980dbbbe2c2c8006d16bd58a7
Roman Potemin <poteminr@gmail.com>
2023-08-16 21:26:27 +0700

    extend llamacpp for new datasets

--- instruction_ner/inference_instruct_llamacpp.py
+++ instruction_ner/inference_instruct_llamacpp.py
@@ -1,17 +1,19 @@
 import re
+import os
 import argparse
 import pandas as pd
 from tqdm import tqdm
 from transformers import GenerationConfig
 from llama_cpp import Llama
 from utils.rudrec.rudrec_reader import create_train_test_instruct_datasets
-from utils.rudrec.rudrec_utis import ENTITY_TYPES
+from utils.nerel_bio.nerel_reader import create_instruct_dataset
 from metric import extract_classes
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--rudrec_path", default='data/rudrec/rudrec_annotated.json', type=str, help='train file_path')
+    parser.add_argument("--dataset_name", default='rudrec', type=str, help='name of dataset')
+    parser.add_argument("--data_path", default='data/rudrec/rudrec_annotated.json', type=str, help='train file_path')
     parser.add_argument("--model_path", default='poteminr/llama2-rudrec', type=str, help='ggml model path')
     parser.add_argument("--model_name", default='poteminr/llama2-rudrec', type=str, help='model name from hf')
     parser.add_argument("--prediction_path", default='prediction.json', type=str, help='path for saving prediction')
@@ -29,9 +31,15 @@ if __name__ == "__main__":
     generation_config = GenerationConfig.from_pretrained(arguments.model_name)
     max_new_tokens = arguments.max_new_tokens
     
-    _, test_dataset = create_train_test_instruct_datasets(arguments.rudrec_path)
-    if arguments.max_instances != -1 and arguments.max_instances < len(test_dataset):
-        test_dataset = test_dataset[:arguments.max_instances]
+    if arguments.dataset_name == 'rudrec': 
+        from utils.rudrec.rudrec_utis import ENTITY_TYPES
+        _, test_dataset = create_train_test_instruct_datasets(arguments.data_path)
+        if arguments.max_instances != -1 and arguments.max_instances < len(test_dataset):
+            test_dataset = test_dataset[:arguments.max_instances]
+    else:
+        from utils.nerel_bio.nerel_bio_utils import ENTITY_TYPES
+        test_path = os.path.join(arguments.data_path, 'test')
+        test_dataset = create_instruct_dataset(test_path, max_instances=arguments.max_instances)
     
     extracted_list = []
     target_list = []
